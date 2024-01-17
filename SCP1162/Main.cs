20b414a8320d1dc9e69653a0f29eb6ec006f11ee
SPL <myuselimyan.a@gmail.com>
2024-01-17 17:01:21 +0300

    test 1

---
+++ SCP1162/Main.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+using Exiled.API.Features;
+using Exiled.API.Features.Doors;
+using Exiled.CustomItems.API.Features;
+using MapEditorReborn.API.Features;
+using UnityEngine;
+
+namespace SCP1162
+{
+    public class Main : Plugin<Config>
+    {
+        public static Main Plugin { get; private set; }
+        public override string Author { get; } = "ShoulHate";
+        public override string Name { get; } = "SCP1162";
+        public override Version Version { get; } = new Version(1, 0, 0);
+        public override Version RequiredExiledVersion { get; } = new Version(8, 7, 0);
+
+        public override void OnEnabled()
+        {
+            Plugin = this;
+
+            CustomItem.RegisterItems(overrideClass: new Scp1162());
+
+            Exiled.Events.Handlers.Server.RoundStarted += OnRoundStarted;
+
+            base.OnEnabled();
+        }
+        public override void OnDisabled()
+        {
+            CustomItem.UnregisterItems();
+            Exiled.Events.Handlers.Server.RoundStarted -= OnRoundStarted;
+
+            base.OnDisabled();
+        }
+
+        private void OnRoundStarted()
+        {
+            Door door = Door.Get(Exiled.API.Enums.DoorType.Scp173Armory);
+            Vector3 vectorAdd = Vector3.zero;
+            int eul = (int)door.Transform.eulerAngles.y;
+
+            if (eul == 0) vectorAdd = new Vector3(-0.5f, 1, 4);
+            else if (eul == 89) vectorAdd = new Vector3(4, 1, 0.5f);
+            else if (eul == 179) vectorAdd = new Vector3(0.5f, 1, -4);
+            else vectorAdd = new Vector3(-4, 1, -0.5f);
+
+            ObjectSpawner.SpawnSchematic("scp1162", position: door.Position + vectorAdd, rotation: door.Rotation);
+        }
+    }
+}
---
+++ SCP1162/Scp1162.cs
@@ -0,0 +1,47 @@
+﻿
+using Exiled.API.Features.Attributes;
+using Exiled.API.Features.Spawn;
+using Exiled.CustomItems.API.Features;
+using Exiled.Events.EventArgs.Player;
+using System;
+
+
+namespace SCP1162
+{
+    [CustomItem(ItemType.Medkit)]
+    public class Scp1162 : CustomItem
+    {
+        public override uint Id { get; set; } = 100;
+        public override string Name { get; set; } = "Scp1162";
+        public override string Description { get; set; }
+        public override float Weight { get; set; } = 5;
+        public override SpawnProperties SpawnProperties { get; set; }
+
+        private Random random = new Random();
+
+        protected override void SubscribeEvents()
+        {
+            base.SubscribeEvents();
+        }
+        protected override void UnsubscribeEvents()
+        {
+            base.UnsubscribeEvents();
+        }
+
+        protected override void OnPickingUp(PickingUpItemEventArgs ev)
+        {
+            if (ev.Player.CurrentItem == null)
+            {
+                ev.Player.EnableEffect(Exiled.API.Enums.EffectType.SeveredHands);
+            }
+            else
+            {
+                ev.Player.CurrentItem.Destroy();
+                ev.Player.AddItem((ItemType)random.Next(51));
+            }
+
+            ev.IsAllowed = false;
+            base.OnPickingUp(ev);
+        }
+    }
+}
